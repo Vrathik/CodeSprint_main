95da34e103a06b74b5433b79889ac1f9f2571f16
AbhignaGowda <144609261+AbhignaGowda@users.noreply.github.com>
2025-01-20 22:04:54 +0530

    .
    .

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: process.env.NEXT_PUBLIC_DATABASE_URL,
+      connectionString:
+      process.env.NEXT_PUBLIC_DATABASE_URL,
+    },
+  };
+  
